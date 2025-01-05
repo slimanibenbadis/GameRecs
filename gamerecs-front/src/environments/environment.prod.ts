c2eb61aac50bdac94db05d5f904166fdeb87e5e9
Benbadis SLIMANI <slimani.b@gmail.com>
2025-01-05 17:13:12 +0100

    Add production environment configuration file

---
+++ gamerecs-front/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  testing: false,
+  apiUrl: '/api'  // In production, we use relative path since both frontend and backend are served from the same domain
+}; 
