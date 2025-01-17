c28a408786988cc02de094d73a408a4d1af475e4
Benbadis SLIMANI <slimani.b@gmail.com>
2025-01-17 21:09:13 +0100

    Add Docker entrypoint script and update configurations for nginx and Dockerfile
    - Introduced a new docker-entrypoint.sh script to replace environment variables in the nginx configuration, enhancing flexibility for deployment.
    - Updated the Dockerfile to use Node.js version 22, ensuring compatibility with the latest features and improvements.
    - Modified nginx.conf to utilize an environment variable for the backend URL, improving the proxy configuration and allowing for dynamic backend routing.
    
    These changes aim to streamline the deployment process and improve the application's configuration management.

---
+++ gamerecs-front/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Replace environment variables in nginx config
+envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
+mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
+
+# Execute CMD
+exec "$@" 
