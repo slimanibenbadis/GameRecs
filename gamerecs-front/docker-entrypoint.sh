ecf61241dd90cdf6e6c1c5db149ef5e34fde3a0f
Benbadis SLIMANI <slimani.b@gmail.com>
2025-02-14 18:49:05 +0100

    Add BACKEND_HOST environment variable support in Nginx configuration
    - Updated docker-entrypoint.sh to include BACKEND_HOST in envsubst
    - Modified nginx.conf to use ${BACKEND_HOST} for proxy Host header
    - Improved backend proxy configuration to support dynamic host configuration

--- gamerecs-front/docker-entrypoint.sh
+++ gamerecs-front/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Replace environment variables in nginx config
-envsubst '${BACKEND_URL} ${PORT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
+envsubst '${BACKEND_URL} ${BACKEND_HOST} ${PORT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
 mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
 
 # Replace environment variables in JS files
